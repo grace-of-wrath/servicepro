087a26ef54f4b9b2f8ff5014b8ca54b26a39a45d
grace-of-wrath <135581863+grace-of-wrath@users.noreply.github.com>
2024-02-28 10:16:47 -0700

    copied all preexisting documents from codeacademy to local repo

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Service Pro Homepage</title>
+  <meta name="Window" content="width=device-width,initial-scale=1" >
+  <link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class id = "titledisplay">
+  <h1>Service Pro Community</h1>
+</div>
+
+<div class id = "subtitle">
+  <h2>Reinventing The Gig Economy</h2>
+</div>
+
+<div class = "navbar">
+  <ul> 
+    <li><a href = "./index.html">Home</a></li>
+    <li><a href ="./app.html">Our App</a></li>
+    <li><a href = "./services.html">Services Offered</a></li>
+    <li><a href = "./resources.html">Additional Resources</a></li>
+    <li><a href = "./contact.html">Contact</a></li>
+  </ul>
+</div>
